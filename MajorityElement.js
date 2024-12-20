fd61e126d57c5b4574944989dfdd098a69f619a6
fatma-dal <fatmadalhoumi123@gmail.com>
2024-12-20 19:12:59 +0100

    leetcode

---
+++ MajorityElement.js
@@ -0,0 +1,17 @@
+function majorityElement(nums) {
+    let candidate = null;
+    let count = 0;
+
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+
+    return candidate;
+}
+
+
+console.log(majorityElement([3, 2, 3])); // Output: 3
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2
